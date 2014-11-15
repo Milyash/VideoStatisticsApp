df93de862263058fe61bc27618a46b65e2c52e6b
leovetter <vetter.leo@gmail.com>
2014-11-15 18:29:57 +0100

    moongoose and first get ok

--- src/Api/app.js
+++ src/Api/app.js
@@ -1,14 +1,46 @@
 var express = require('express')
+var mongoose   = require('mongoose');
+
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var port = process.env.PORT || 8080;        // set our port
 
-var server = app.listen(3000, function () {
+mongoose.connect('mongodb://localhost:27017');
+
+var Video     = require('./models/video');
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// on routes that end in /videos
+// ----------------------------------------------------
+router.route('/videos') 
+    // get all the videos (accessed at GET http://localhost:8080/api/videos)
+    .get(function(req, res) {
+
+        Video.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+var server = app.listen(port, function () {
 
   var host = server.address().address
-  var port = server.address().port
 
   console.log('Api app listening at http://%s:%s', host, port)
 
---
+++ src/Api/models/video.js
@@ -0,0 +1,8 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var VideoSchema   = new Schema({
+	url: String
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
