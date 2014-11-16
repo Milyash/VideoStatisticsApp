17a5ac0d8135851f9a063bf9a803022da5151123
leovetter <vetter.leo@gmail.com>
2014-11-16 19:27:32 +0100

    first basic post working

--- src/Api/app.js
+++ src/Api/app.js
@@ -1,7 +1,20 @@
 var express = require('express')
 var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express()
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -31,12 +44,25 @@ router.route('/videos')
 
             res.json(bears);
         });
+    })
+    // create a video (accessed at POST http://localhost:8080/api/videos)
+    .post(function(req, res) {
+        
+        var video = new Video();      // create a new instance of the Video model
+        video.url = req.body.url;  // set the videos url
+
+        // save the bear and check for errors
+        video.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Video created!' });
+        });
     });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
-
 
 var server = app.listen(port, function () {
 
